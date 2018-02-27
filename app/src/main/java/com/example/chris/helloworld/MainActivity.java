7e5b3063bde7300afc54eab50625b3d4847f31b0
Christopher Ras <chris@MacBook-Pro-2.local>
2018-02-27 13:15:39 +0100

    first commit

---
+++ app/src/main/java/com/example/chris/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.chris.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.io.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+
+}
